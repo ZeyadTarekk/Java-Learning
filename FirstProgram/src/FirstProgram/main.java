21b659386206906fc40be59931a684f311cd4d44
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-03-17 10:47:42 +0200

    First program done

--- FirstProgram/src/FirstProgram/main.java
+++ FirstProgram/src/FirstProgram/main.java
@@ -2,4 +2,11 @@ package FirstProgram;
 
 public class main {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Zeyad");
+		System.out.println("Hello, World!");
+		
+	}
+
 }
