b6a621b3ac8e5582ba3785befa505b297b51b46d
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-03-18 18:26:26 +0200

    Variables and data types first part

--- SecondProgram/src/MainPakage/mainFunction.java
+++ SecondProgram/src/MainPakage/mainFunction.java
@@ -2,4 +2,14 @@ package MainPakage;
 
 public class mainFunction {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int helloWorld = 5;
+		double decimalPointNumber = 3.6;
+//		boolean
+		System.out.println(helloWorld);
+		System.out.println(decimalPointNumber);
+		
+	}
+
 }
