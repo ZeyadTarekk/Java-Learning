c66ebfd586d60689554443316cb1be84dcb003f8
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-03-20 13:29:18 +0200

    Third Program

--- ThirdProgram/src/MainPackage/MainClass.java
+++ ThirdProgram/src/MainPackage/MainClass.java
@@ -3,5 +3,10 @@ package MainPackage;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello From intellij");
+        int x = 7;
+        int y = 57;
+        double z = (double) y / x;
+        double power = Math.pow(2,3);
+        System.out.println(power);
     }
 }
