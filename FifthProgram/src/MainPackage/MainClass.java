1bf8a17867715426d64e0fb26b632abfc3e892dd
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-03-20 15:54:11 +0200

    Comparing strings

--- FifthProgram/src/MainPackage/MainClass.java
+++ FifthProgram/src/MainPackage/MainClass.java
@@ -1,7 +1,11 @@
 package MainPackage;
-
+import java.util.Scanner;
 public class MainClass {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String newStr = sc.nextLine();
+        System.out.println(newStr);
+        boolean value = newStr.equals("Zeyad");
+        System.out.println(value);
     }
 }
