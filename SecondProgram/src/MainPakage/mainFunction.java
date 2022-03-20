dd2bf203e5bbd1d71f5f1445ec0977805d39b03b
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-03-20 12:14:24 +0200

    More about Data types

--- SecondProgram/src/MainPakage/mainFunction.java
+++ SecondProgram/src/MainPakage/mainFunction.java
@@ -6,10 +6,21 @@ public class mainFunction {
 		// TODO Auto-generated method stub
 		int helloWorld = 5;
 		double decimalPointNumber = 3.6;
-//		boolean
+		boolean b = false;
+		boolean c = true;
+		char firstLetter = 'A';
+		String str = "Zeyad Tarek";
 		System.out.println(helloWorld);
 		System.out.println(decimalPointNumber);
-		
+		System.out.println("Hello world");
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(firstLetter);
+		System.out.println(str);
+		b = true;
+		System.out.println(b);
+		str = "Ahmed Mohamed";
+		System.out.println(str);
 	}
 
 }
