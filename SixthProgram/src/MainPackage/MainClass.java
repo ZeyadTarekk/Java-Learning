761e4b817f800ee946c2ed55a47c3def9c97c9dc
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-03-20 16:30:00 +0200

    Arrays

--- SixthProgram/src/MainPackage/MainClass.java
+++ SixthProgram/src/MainPackage/MainClass.java
@@ -1,4 +1,11 @@
 package MainPackage;
 
 public class MainClass {
+    public static void main(String[] args) {
+        String[] newArr = new String[5];
+        newArr[0] = "Zeyad";
+        System.out.println(newArr[1]);
+        newArr[2] = "Hello";
+        System.out.println(newArr[2]);
+    }
 }
